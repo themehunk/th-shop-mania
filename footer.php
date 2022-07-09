b8ffde4c8052d67e4063826f7d4dba1224d88d13
iheeresh <heereshd@gmail.com>
2022-07-09 17:01:22 +0530

    header conditions work

--- footer.php
+++ footer.php
@@ -8,9 +8,9 @@
  * @since 1.0.0
  */ 
 global $post;
-if ((is_single() || is_page()) || ((class_exists('WooCommerce')) && (is_product() || is_checkout() || is_cart() || is_account_page()))
+if ((is_single() || is_page()) || ((class_exists('WooCommerce')) && (is_woocommerce() || is_checkout() || is_cart() || is_account_page()))
 ||  is_front_page() || is_home()) {
-    if (is_shop()) {
+    if (class_exists('WooCommerce') && is_shop()) {
         $shop_page_id = get_option('woocommerce_shop_page_id');
         $postid = $shop_page_id;
     } elseif(th_shop_mania_is_blog()){
