026ef1ef16f02beee533aff6716f64103cd9f897
iheeresh <heereshd@gmail.com>
2023-01-17 14:16:51 +0530

    about pattern

--- inc/patterns/about.php
+++ inc/patterns/about.php
@@ -10,15 +10,19 @@
 return array(
     'title'      =>__( 'About Section', 'th-shop-mania' ),
     'categories' => array( 'blockline' ),
-    'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"},"blockGap":"29px"}},"backgroundColor":"background","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background-background-color has-background" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:columns {"verticalAlignment":"center"} -->
-<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"39%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:39%"><!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":1.2,"fontStyle":"normal","fontWeight":"600","letterSpacing":"2px"},"color":{"text":"#2e3d62"}},"fontSize":"huge"} -->
-<h1 class="has-text-color has-huge-font-size" style="color:#2e3d62;font-style:normal;font-weight:600;letter-spacing:2px;line-height:1.2">Startup Company</h1>
+    'content'    => '<!-- wp:paragraph -->
+<p>h</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"},"blockGap":"29px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:columns {"verticalAlignment":"center","style":{"color":{"background":"#fafafa"},"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center has-background" style="background-color:#fafafa;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:column {"verticalAlignment":"center","width":"39%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:39%"><!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":1.2,"fontStyle":"normal","fontWeight":"600","letterSpacing":"2px"},"color":{"text":"#2e3d62"}},"fontSize":"extra-large"} -->
+<h1 class="has-text-color has-extra-large-font-size" style="color:#2e3d62;font-style:normal;font-weight:600;letter-spacing:2px;line-height:1.2">Startup Company</h1>
 <!-- /wp:heading -->
 
-<!-- wp:heading {"style":{"typography":{"fontSize":"36px","fontStyle":"normal","fontWeight":"600","letterSpacing":"1px"},"color":{"text":"#2e3d62"}}} -->
-<h2 class="has-text-color" style="color:#2e3d62;font-size:36px;font-style:normal;font-weight:600;letter-spacing:1px">Our best you can get</h2>
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"1px"},"color":{"text":"#2e3d62"}},"fontSize":"normal"} -->
+<h2 class="has-text-color has-normal-font-size" style="color:#2e3d62;font-style:normal;font-weight:600;letter-spacing:1px">Our best you can get</h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"40px"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
@@ -35,10 +39,10 @@ return array(
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"61%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:61%"><!-- wp:group {"style":{"border":{"radius":{"topLeft":"10px","bottomLeft":"2917px"},"width":"47px"},"spacing":{"blockGap":"0","padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"}}},"backgroundColor":"vivid-red","textColor":"vivid-red","layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group has-vivid-red-color has-vivid-red-background-color has-text-color has-background" style="border-width:47px;border-top-left-radius:10px;border-bottom-left-radius:2917px;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem"><!-- wp:image {"align":"right","id":245,"sizeSlug":"full","linkDestination":"none","style":{"border":{"width":"0px","style":"none"}}} -->
-<figure class="wp-block-image alignright size-full has-custom-border"><img src="https://demo.themehunk.com/wp-content/uploads/2023/01/herok-1.png" alt="" class="wp-image-245" style="border-style:none;border-width:0px"/></figure>
-<!-- /wp:image --></div>
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:61%"><!-- wp:group {"style":{"border":{"radius":{"topLeft":"10px","bottomLeft":"2917px"},"width":"47px"},"spacing":{"blockGap":"0","padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"}}},"backgroundColor":"vivid-red","textColor":"vivid-red","layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+<div class="wp-block-group has-vivid-red-color has-vivid-red-background-color has-text-color has-background" style="border-width:47px;border-top-left-radius:10px;border-bottom-left-radius:2917px;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem"><!-- wp:group {"style":{"border":{"radius":"100px"},"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"}}},"backgroundColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+<div class="wp-block-group has-white-background-color has-background" style="border-radius:100px;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
--- inc/patterns/service.php
+++ inc/patterns/service.php
@@ -10,14 +10,14 @@
 return array(
     'title'      =>__( 'Service Section', 'th-shop-mania' ),
     'categories' => array( 'blockline' ),
-    'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|50","bottom":"var:preset|spacing|80","left":"var:preset|spacing|50"}}},"backgroundColor":"vivid-red","layout":{"type":"constrained"}} -->
+    'content'    => '<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|50","bottom":"var:preset|spacing|80","left":"var:preset|spacing|50"}}},"backgroundColor":"vivid-red","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-vivid-red-background-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--50)"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"color":{"background":"#ff3c3c"}}} -->
-<div class="wp-block-column has-background" style="background-color:#ff3c3c;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"id":697,"width":72,"height":72,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="https://demo.themehunk.com/wp-content/uploads/2023/01/BALL.png" alt="" class="wp-image-697" width="72" height="72"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"textColor":"background","fontSize":"medium"} -->
+<div class="wp-block-column has-background" style="background-color:#ff3c3c;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"textColor":"background","fontSize":"medium"} -->
 <h2 class="has-background-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:700;text-transform:uppercase">Ideas &amp; Concepts</h2>
 <!-- /wp:heading -->
 
@@ -31,12 +31,8 @@ return array(
 <!-- /wp:column -->
 
 <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"color":{"background":"#f6ff9e"}}} -->
-<div class="wp-block-column has-background" style="background-color:#f6ff9e;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"id":698,"width":72,"height":72,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="http://demo.themehunk.com/wp-content/uploads/2023/01/2.png" alt="" class="wp-image-698" width="72" height="72"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"textColor":"foreground","fontSize":"medium"} -->
-<h2 class="has-foreground-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:700;text-transform:uppercase">SEO Specialists</h2>
+<div class="wp-block-column has-background" style="background-color:#f6ff9e;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"textColor":"black","fontSize":"medium"} -->
+<h2 class="has-black-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:700;text-transform:uppercase">SEO Specialists</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"letterSpacing":"1px"},"color":{"text":"#0f0f0f"}},"fontSize":"extra-small"} -->
@@ -49,11 +45,7 @@ return array(
 <!-- /wp:column -->
 
 <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"color":{"background":"#ff3c3c"}}} -->
-<div class="wp-block-column has-background" style="background-color:#ff3c3c;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"id":697,"width":72,"height":72,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="https://demo.themehunk.com/wp-content/uploads/2023/01/BALL.png" alt="" class="wp-image-697" width="72" height="72"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"textColor":"background","fontSize":"medium"} -->
+<div class="wp-block-column has-background" style="background-color:#ff3c3c;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"textColor":"background","fontSize":"medium"} -->
 <h2 class="has-background-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:700;text-transform:uppercase">East to Use</h2>
 <!-- /wp:heading -->
 
