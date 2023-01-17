8404cb0c4e6113caeafb75001738a019cae4eb75
iheeresh <heereshd@gmail.com>
2023-01-17 18:26:51 +0530

    about

--- inc/patterns/about.php
+++ inc/patterns/about.php
@@ -10,29 +10,25 @@
 return array(
     'title'      =>__( 'About Section', 'th-shop-mania' ),
     'categories' => array( 'thshopmania' ),
-    'content'    => '<!-- wp:paragraph -->
-<p>h</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"},"blockGap":"29px"}},"layout":{"type":"constrained"}} -->
+    'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"},"blockGap":"29px"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:columns {"verticalAlignment":"center","style":{"color":{"background":"#fafafa"},"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
 <div class="wp-block-columns are-vertically-aligned-center has-background" style="background-color:#fafafa;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:column {"verticalAlignment":"center","width":"39%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:39%"><!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":1.2,"fontStyle":"normal","fontWeight":"600","letterSpacing":"2px"},"color":{"text":"#2e3d62"}},"fontSize":"extra-large"} -->
-<h1 class="has-text-color has-extra-large-font-size" style="color:#2e3d62;font-style:normal;font-weight:600;letter-spacing:2px;line-height:1.2">'.esc_html__('Startup Company','th-shop-mania').'</h1>
+<h1 class="has-text-color has-extra-large-font-size" style="color:#2e3d62;font-style:normal;font-weight:600;letter-spacing:2px;line-height:1.2">Startup Company</h1>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"1px"},"color":{"text":"#2e3d62"}},"fontSize":"normal"} -->
-<h2 class="has-text-color has-normal-font-size" style="color:#2e3d62;font-style:normal;font-weight:600;letter-spacing:1px">'.esc_html__('Our best you can get','th-shop-mania').'</h2>
+<h2 class="has-text-color has-normal-font-size" style="color:#2e3d62;font-style:normal;font-weight:600;letter-spacing:1px">Our best you can get</h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"40px"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group" style="margin-top:40px"><!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"white","style":{"border":{"radius":"25px"},"color":{"text":"#4167cf"}},"className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-white-background-color has-text-color has-background wp-element-button" style="border-radius:25px;color:#4167cf">'.esc_html__('Explore More','th-shop-mania').'</a></div>
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-white-background-color has-text-color has-background wp-element-button" style="border-radius:25px;color:#4167cf">Explore More</a></div>
 <!-- /wp:button -->
 
 <!-- wp:button {"style":{"border":{"radius":"25px"},"color":{"background":"#4167cf"}}} -->
-<div class="wp-block-button"><a class="wp-block-button__link has-background wp-element-button" style="border-radius:25px;background-color:#4167cf">'.esc_html__('Get Started Today','th-shop-mania').'</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link has-background wp-element-button" style="border-radius:25px;background-color:#4167cf">Get Started Today</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
