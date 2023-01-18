685554629dc7ee6af2eb9642faf6a4a6d91309d5
iheeresh <heereshd@gmail.com>
2023-01-18 13:02:31 +0530

    testimonial & banner ribon added

---
+++ inc/patterns/testimonials.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Testimonial Section
+ * 
+ * slug: ribbon
+ * title: Testimonial Section
+ * categories: thshopmania
+ */
+
+return array(
+    'title'      =>__( 'Testimonial Section', 'th-shop-mania' ),
+    'categories' => array( 'thshopmania' ),
+    'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|30","bottom":"var:preset|spacing|60","left":"var:preset|spacing|30"}}},"gradient":"midnight","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-midnight-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"luminous-vivid-amber","fontSize":"medium"} -->
+<h2 class="has-luminous-vivid-amber-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:500">Clients &amp; Testimonial</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"background","fontSize":"large"} -->
+<p class="has-background-color has-text-color has-large-font-size">We are Trusted by more than 14k Clients</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background","fontSize":"small"} -->
+<p class="has-background-color has-text-color has-small-font-size" style="font-style:normal;font-weight:300">Proin in libero luctus neque sodales fermentum ac quis metus. Curabitur facilisis leo vitae lectus volutpat, id vestibulum justo pulvina</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"color":{"background":"#4167cf"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-background wp-element-button" style="background-color:#4167cf">Show More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:columns {"verticalAlignment":"top"} -->
+<div class="wp-block-columns are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"color":{"background":"#4167cf"}},"layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-top has-background" style="background-color:#4167cf;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"align":"left","style":{"typography":{"lineHeight":"1.4"}},"textColor":"background","fontSize":"small"} -->
+<p class="has-text-align-left has-background-color has-text-color has-small-font-size" style="line-height:1.4">Quisque pharetra massa nec nisi placerat, vitae dapibus tortor dictum. Vestibulum li Quisque pharetra massa nec nisi placerat, vitae dapibus tortor dictum. Vestibulum li</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"lineHeight":"0.8"}},"textColor":"luminous-vivid-amber"} -->
+<h2 class="has-luminous-vivid-amber-color has-text-color" style="line-height:0.8">Maria</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"0.2"}},"textColor":"background","fontSize":"tiny"} -->
+<p class="has-background-color has-text-color has-tiny-font-size" style="line-height:0.2">Analyst</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"color":{"background":"#4167cf"}},"layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-top has-background" style="background-color:#4167cf;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"align":"left","style":{"typography":{"lineHeight":"1.4"}},"textColor":"background","fontSize":"small"} -->
+<p class="has-text-align-left has-background-color has-text-color has-small-font-size" style="line-height:1.4">Quisque pharetra massa nec nisi placerat, vitae dapibus tortor dictum. Vestibulum li Quisque pharetra massa nec nisi placerat, vitae dapibus tortor dictum. Vestibulum li</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"lineHeight":"0.8"}},"textColor":"luminous-vivid-amber"} -->
+<h2 class="has-luminous-vivid-amber-color has-text-color" style="line-height:0.8">Rama</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"0.2"}},"textColor":"background","fontSize":"tiny"} -->
+<p class="has-background-color has-text-color has-tiny-font-size" style="line-height:0.2">Manager</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->',
+);
